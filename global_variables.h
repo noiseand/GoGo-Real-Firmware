812b4151a7965dc6aa6caf17e44de3112316a01b
Lucas Tanure <ltanure@gmail.com>
2014-01-26 14:38:06 -0200

    Variable to make beep in timer2 interrupt

--- global_variables.h
+++ global_variables.h
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with GoGo Real.  If not, see <http://www.gnu.org/licenses/>.
  */
+int1 beep_is_high = 0;
 
 //Usb Buffer
 unsigned int8 usbBuffer[USB_BUFFER_SIZE];
